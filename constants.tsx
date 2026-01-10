b946ef09deb0d5a0c434e1493c0020eb93abea8c
Madipadige-ManishKumar <oomanish459@gmail.com>
2026-01-10 18:46:42 +0530

    Fix image URL case sensitivity in constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -56,7 +56,7 @@ export const PROJECTS: Project[] = [
       ' Implemented ML algorithms (Logistic Regression, Decision Trees, SVM) to predict diabetes, lung, liver, and blood pressure conditions, achieving Engineered a Flask-based web application',
     tags: ['Python', 'Machine Learning', 'Flask', 'Scikit-learn'],
     link: 'https://github.com/Madipadige-ManishKumar/AIHealthpro',
-    imageUrl: './assets/AIHealthPro.png',
+    imageUrl: './assets/AIhealthPro.png',
   },
 
 ];
